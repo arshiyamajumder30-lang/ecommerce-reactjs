e349764c4bf1429e6f7f1815d59e3fd31c1b5199
Arshiya Majumder <arshiyamajumder30@gmail.com>
2026-07-08 16:48:56 +0530

    create multiple pgs usig React-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,14 +1,19 @@
-
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage' //use HomePage() here
 
 
 import './App.css'
 
 function App() {
- 
 
   return (
-<HomePage /> //inserts HomePage() component into our App()
+    //inside <App> add <Routes> component 
+//path="/" -> index
+<Routes>
+<Route path="/" element={<HomePage />}></Route>
+<Route path="checkout" element={<div>checkout pg</div>}></Route>
+</Routes>
+//displays HomePage on going to the given url path
   );
 }
 
