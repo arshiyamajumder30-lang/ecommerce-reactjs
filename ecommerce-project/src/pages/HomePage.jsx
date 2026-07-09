8280bf6acfe28f75bc15185a83ff5642380d514b
Arshiya Majumder <arshiyamajumder30@gmail.com>
2026-07-09 16:41:10 +0530

    generate HTML for HomePage using react

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,87 +2,41 @@
 import { Header } from '../compo/Header';
 import './HomePage.css';
 //to import the css for HomePage
-
+import { products } from '../../start-code/data/products'; //imports products array from product.js file 
 
 //has React code for the home page
 //creates compo for the homepg
 export function HomePage(){
 //returs all html code from home pg(copy code from idex.html)
-//wrap in fragment to return only 1 element
+//wrap in fragment to return only 1 elementss
 return(
 <>
 <title>Ecommerce Project</title>
 <Header />
     <div className="home-page">
       <div className="products-grid">
-        <div className="product-container">
+        {products.map((product) => {
+return (
+        <div key= {product.id} 
+        className="product-container">
           <div className="product-image-container">
             <img className="product-image"
-              src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+              src={product.image} />
           </div>
           <div className="product-name limit-text-to-2-lines">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            {product.name}
           </div>
 
           <div className="product-rating-container">
             <img className="product-rating-stars"
-              src="images/ratings/rating-45.png" />
+              src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
             <div className="product-rating-count link-primary">
-              87
+              {product.rating.count}
             </div>
           </div>
 
           <div className="product-price">
-            $10.90
-          </div>
-
-          <div className="product-quantity-container">
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div classNameName="product-spacer"></div>
-
-          <div className="added-to-cart">
-            <img src="images/icons/checkmark.png" />
-            Added
-          </div>
-
-          <button className="add-to-cart-button button-primary">
-            Add to Cart
-          </button>
-        </div>
-
-        <div className="product-container">
-          <div className="product-image-container">
-            <img className="product-image"
-              src="images/products/intermediate-composite-basketball.jpg" />
-          </div>
-
-          <div className="product-name limit-text-to-2-lines">
-            Intermediate Size Basketball
-          </div>
-
-          <div className="product-rating-container">
-            <img className="product-rating-stars"
-              src="images/ratings/rating-40.png" />
-            <div className="product-rating-count link-primary">
-              127
-            </div>
-          </div>
-
-          <div className ="product-price">
-            $20.95
+            ${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div className="product-quantity-container">
@@ -111,57 +65,11 @@ return(
             Add to Cart
           </button>
         </div>
+);
+        })}
+</div>
 
-        <div className="product-container">
-          <div className="product-image-container">
-            <img className="product-image"
-              src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-          </div>
-
-          <div className="product-name limit-text-to-2-lines">
-            Adults Plain Cotton T-Shirt - 2 Pack
-          </div>
-
-          <div className="product-rating-container">
-            <img className="product-rating-stars"
-              src="images/ratings/rating-45.png" />
-            <div className="product-rating-count link-primary">
-              56
-            </div>
-          </div>
-
-          <div className="product-price">
-            $7.99
-          </div>
-
-          <div className="product-quantity-container">
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div className="product-spacer"></div>
-
-          <div className="added-to-cart">
-            <img src="images/icons/checkmark.png" />
-            Added
-          </div>
-
-          <button className="add-to-cart-button button-primary">
-            Add to Cart
-          </button>
-        </div>
-      </div>
-    </div>
+</div>
 </>
 );
 }
